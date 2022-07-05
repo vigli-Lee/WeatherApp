0ba318361287f971b1438e61bc48f8f2dbe7f649
vigli-Lee <wnsqjadl@gmail.com>
2022-07-05 22:24:02 +0900

    add Stetho Network Trace

--- app/src/main/java/com/plcoding/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/plcoding/weatherapp/WeatherApp.kt
@@ -1,7 +1,18 @@
 package com.plcoding.weatherapp
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WeatherApp: Application()
+class WeatherApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initStetho()
+    }
+
+    private fun initStetho() {
+        Stetho.initializeWithDefaults(this)
+    }
+}
--- app/src/main/java/com/plcoding/weatherapp/di/AppModule.kt
+++ app/src/main/java/com/plcoding/weatherapp/di/AppModule.kt
@@ -1,6 +1,8 @@
 package com.plcoding.weatherapp.di
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.plcoding.weatherapp.data.remote.WeatherApi
@@ -8,6 +10,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
@@ -20,7 +23,12 @@ object AppModule {
     @Provides
     @Singleton
     fun provideWeatherApi(): WeatherApi {
+        val client = OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
         return Retrofit.Builder()
+            .client(client)
             .baseUrl("https://api.open-meteo.com/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
